78e87105f9161e1532b6839c98fc61754cebcebb
Vyacheslav <vyacheslav@MacBook-Air-fox.local>
2023-02-02 14:22:19 +0200

    Updated android app

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import { createStore, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
 import MainNavigator from './utils/navigation.util'
-import Orientation from 'react-native-orientation-locker';
 
 
 export default class App extends Component {
 
   componentDidMount() {
-		Orientation.lockToPortrait();
 	}
 
   render () {
